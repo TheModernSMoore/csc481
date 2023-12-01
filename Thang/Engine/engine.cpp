1bde808d63d189ecdb247b23ad76c72356e68e79
TheModernSMoore <themodernsmoore@gmail.com>
2023-12-01 00:00:24 -0500

    xeyes my beloved

--- Thang/Engine/engine.cpp
+++ Thang/Engine/engine.cpp
@@ -6,6 +6,8 @@
 #include <zmq_addon.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <v8.h>
+#include <libplatform/libplatform.h>
 
 using json = nlohmann::json;
 
@@ -110,137 +112,205 @@ void clientCreation(int *clients_connected, std::vector<std::string> *inputs, st
 
 int main(int argc, char const *argv[])
 {
-    // sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window", sf::Style::Resize);
-    // // change the position of the window (relatively to the desktop)
-    // window.setPosition(sf::Vector2i(50, 50));
+    /*
+     * NOTE: You must initialize v8 in main, otherwise handles will go out of scope.
+     *
+     * Contexts are generally on a per-thread basis, so if you would like to do 
+     * script execution in a different thread, you must create a new context in
+     * the other thread and give care to ensure the context doesn't go out of scope
+     * before the thread's run function is called.
+	 *
+	 * The below v8 function calls are basically boilerplate. 
+     */
+    std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+    v8::V8::InitializePlatform(platform.release());
+    v8::V8::InitializeICU();
+    v8::V8::Initialize();
+    v8::Isolate::CreateParams create_params;
+    create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+    v8::Isolate *isolate = v8::Isolate::New(create_params);
+
+    { // anonymous scope for managing handle scope
+        v8::Isolate::Scope isolate_scope(isolate); // must enter the virtual machine to do stuff
+        v8::HandleScope handle_scope(isolate);
+
+		// Best practice to isntall all global functions in the context ahead of time.
+        v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
+        // // Bind the global 'print' function to the C++ Print callback.
+        // global->Set(isolate, "print", v8::FunctionTemplate::New(isolate, v8helpers::Print));
+		// // Bind the global static factory function for creating new GameObject instances
+		// global->Set(isolate, "gameobjectfactory", v8::FunctionTemplate::New(isolate, GameObject::ScriptedGameObjectFactory));
+		// // Bind the global static function for retrieving object handles
+		// global->Set(isolate, "gethandle", v8::FunctionTemplate::New(isolate, ScriptManager::getHandleFromScript));
+
+        //                      global->Set(isolate, name of function inside of js, v8::Function Template::New(isolate, the function from c++ that will be under the given name))
 
 
-    TimeManager *timeManager = TimeManager::get();
-    Timeline realTime = Timeline();
-    Timeline localTime = Timeline(&realTime, 65000);
-    timeManager->addTimeline(&realTime);
-    timeManager->addTimeline(&localTime);
+        v8::Local<v8::Context> default_context =  v8::Context::New(isolate, NULL, global);
+		v8::Context::Scope default_context_scope(default_context); // enter the context
+
+        ScriptManager *sm = new ScriptManager(isolate, default_context); 
+
+		// Without parameters, these calls are made to the default context
+
+        //                                                These add scripts (.js files) so that they can be called from here (in C++)
+
+        // sm->addScript("hello_world", "scripts/hello_world.js");
+        // sm->addScript("perform_function", "scripts/perform_function.js");
+
+		// Create a new context
+		v8::Local<v8::Context> object_context = v8::Context::New(isolate, NULL, global);
+		sm->addContext(isolate, object_context, "object_context");
+
+		GameObject *go = new GameObject();
+		go->exposeToV8(isolate, object_context);
+
+		// With the "object_context" parameter, these scripts are put in a
+		// different context than the prior three scripts
+		sm->addScript("create_object", "scripts/create_object.js", "object_context");
+		sm->addScript("random_object", "scripts/random_object.js", "object_context");
+		sm->addScript("random_position", "scripts/random_position.js", "object_context");
+		sm->addScript("modify_position", "scripts/modify_position.js", "object_context");
+
+		// Use the following 4 lines in place of the above 4 lines as the
+		// reference if you don't plan to use multiple contexts
+		/* sm->addScript("create_object", "scripts/create_object.js"); */
+		/* sm->addScript("random_object", "scripts/random_object.js"); */
+		/* sm->addScript("random_position", "scripts/random_position.js"); */
+		/* sm->addScript("modify_position", "scripts/modify_position.js"); */
+
+        // sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window", sf::Style::Resize);
+        // // change the position of the window (relatively to the desktop)
+        // window.setPosition(sf::Vector2i(50, 50));
 
 
-    ObjectManager *objectManager = ObjectManager::get();
-
-    Platform morb(sf::Vector2f(120.f, 25.f));
-    morb.setPosition(215.f, 250.f);
-    std::vector<sf::Vector2f> morb_set;
-    morb_set.push_back(sf::Vector2f(215.f, 450.f));
-    morb_set.push_back(sf::Vector2f(215.f, 250.f));
-    morb.setToGo(morb_set);
-    morb.setSpeed(2);
-    morb.setFillColor(sf::Color::Blue);
-
-    objectManager->addObject(&morb);
-
-    Platform borb(sf::Vector2f(120.f, 25.f));
-    borb.setPosition(375.f, 450.f);
-    std::vector<sf::Vector2f> borb_set;
-    borb_set.push_back(sf::Vector2f(575.f, 450.f));
-    borb_set.push_back(sf::Vector2f(575.f, 350.f));
-    borb_set.push_back(sf::Vector2f(375.f, 350.f));
-    borb_set.push_back(sf::Vector2f(375.f, 450.f));
-    borb.setToGo(borb_set);
-    borb.setSpeed(2);
-    borb.setFillColor(sf::Color(166, 126, 18));
-
-    objectManager->addObject(&borb);
-
-    Platform ground(sf::Vector2f(600.f, 25.f));
-    ground.setPosition(0, 600 - 25);
-    ground.setTexturePath("img/grass.jpg");
-    
-
-    objectManager->addObject(&ground);
-
-    Platform ground_again(sf::Vector2f(550.f, 25.f));
-    ground_again.setPosition(650, 600 - 25);
-    ground_again.setTexturePath("img/grass.jpg");
-    
-
-    objectManager->addObject(&ground_again);
+        TimeManager *timeManager = TimeManager::get();
+        Timeline realTime = Timeline();
+        Timeline localTime = Timeline(&realTime, 65000);
+        timeManager->addTimeline(&realTime);
+        timeManager->addTimeline(&localTime);
 
 
-    Platform wall(sf::Vector2f(25.f, 600-25));
-    wall.setPosition(100, 0);
-    wall.setFillColor(sf::Color::Red);
+        ObjectManager *objectManager = ObjectManager::get();
 
-    objectManager->addObject(&wall);
+        Platform morb(sf::Vector2f(120.f, 25.f));
+        morb.setPosition(215.f, 250.f);
+        std::vector<sf::Vector2f> morb_set;
+        morb_set.push_back(sf::Vector2f(215.f, 450.f));
+        morb_set.push_back(sf::Vector2f(215.f, 250.f));
+        morb.setToGo(morb_set);
+        morb.setSpeed(2);
+        morb.setFillColor(sf::Color::Blue);
 
-    SpawnPoint spawner;
-    spawner.setPosition(200, 550);
+        objectManager->addObject(&morb);
 
-    objectManager->addObject(&spawner);
+        Platform borb(sf::Vector2f(120.f, 25.f));
+        borb.setPosition(375.f, 450.f);
+        std::vector<sf::Vector2f> borb_set;
+        borb_set.push_back(sf::Vector2f(575.f, 450.f));
+        borb_set.push_back(sf::Vector2f(575.f, 350.f));
+        borb_set.push_back(sf::Vector2f(375.f, 350.f));
+        borb_set.push_back(sf::Vector2f(375.f, 450.f));
+        borb.setToGo(borb_set);
+        borb.setSpeed(2);
+        borb.setFillColor(sf::Color(166, 126, 18));
 
-    SpawnPoint spawner2;
-    spawner2.setPosition(800, 550);
+        objectManager->addObject(&borb);
 
-    objectManager->addObject(&spawner2);
+        Platform ground(sf::Vector2f(600.f, 25.f));
+        ground.setPosition(0, 600 - 25);
+        ground.setTexturePath("img/grass.jpg");
+        
 
-    DeathBox pit(sf::Vector2f(1800.f, 50.f));
-    pit.setPosition(0, 650);
+        objectManager->addObject(&ground);
 
-    objectManager->addObject(&pit);
+        Platform ground_again(sf::Vector2f(550.f, 25.f));
+        ground_again.setPosition(650, 600 - 25);
+        ground_again.setTexturePath("img/grass.jpg");
+        
 
-    std::vector<Character*> characters;
-
-
-    std::mutex m;
-
-
-    std::vector<std::string> inputs;
-    int clients_connected = 0;
-
-    std::thread client_creator(clientCreation, &clients_connected, &inputs, &m, &characters);
-
-
-    EventManager *eventManager = EventManager::get();
-
-    eventManager->addEventToHandler(std::list<EventType> {CHARACTER_COLLISION}, new CharCollideHandler);
-    eventManager->addEventToHandler(std::list<EventType> {CHARACTER_DEATH}, new CharDeathHandler);
-    eventManager->addEventToHandler(std::list<EventType> {CHARACTER_SPAWN}, new CharSpawnHandler);
-    eventManager->addEventToHandler(std::list<EventType> {USER_INPUT}, new InputHandler);
-    eventManager->addEventToHandler(std::list<EventType> {PAUSE}, new PauseHandler);
-    eventManager->addEventToHandler(std::list<EventType> {CYCLE_SPEED}, new SpeedHandler);
+        objectManager->addObject(&ground_again);
 
 
-    // do not ask why this is necessary, IF I WERE TO CREATE 1 MORE OBJECT, THEY WOULD NOT APPEAR
-    // AND THIS FIXED IT
-    for (int i = 0; i < 5; i++)  
-    {
-        realTime.setTime();
-        localTime.setTime();
-    }
+        Platform wall(sf::Vector2f(25.f, 600-25));
+        wall.setPosition(100, 0);
+        wall.setFillColor(sf::Color::Red);
 
-    while (1)
-    {
-        realTime.setTime();
-        // set the new local time with each loop
-        localTime.setTime();
+        objectManager->addObject(&wall);
 
+        SpawnPoint spawner;
+        spawner.setPosition(200, 550);
+
+        objectManager->addObject(&spawner);
+
+        SpawnPoint spawner2;
+        spawner2.setPosition(800, 550);
+
+        objectManager->addObject(&spawner2);
+
+        DeathBox pit(sf::Vector2f(1800.f, 50.f));
+        pit.setPosition(0, 650);
+
+        objectManager->addObject(&pit);
+
+        std::vector<Character*> characters;
+
+
+        std::mutex m;
+
+
+        std::vector<std::string> inputs;
+        int clients_connected = 0;
+
+        std::thread client_creator(clientCreation, &clients_connected, &inputs, &m, &characters);
+
+
+        EventManager *eventManager = EventManager::get();
+
+        eventManager->addEventToHandler(std::list<EventType> {CHARACTER_COLLISION}, new CharCollideHandler);
+        eventManager->addEventToHandler(std::list<EventType> {CHARACTER_DEATH}, new CharDeathHandler);
+        eventManager->addEventToHandler(std::list<EventType> {CHARACTER_SPAWN}, new CharSpawnHandler);
+        eventManager->addEventToHandler(std::list<EventType> {USER_INPUT}, new InputHandler);
+        eventManager->addEventToHandler(std::list<EventType> {PAUSE}, new PauseHandler);
+        eventManager->addEventToHandler(std::list<EventType> {CYCLE_SPEED}, new SpeedHandler);
+
+
+        // do not ask why this is necessary, IF I WERE TO CREATE 1 MORE OBJECT, THEY WOULD NOT APPEAR
+        // AND THIS FIXED IT
+        for (int i = 0; i < 5; i++)  
         {
-            std::unique_lock<std::mutex> lock(m);
-            int idx = 0;
-            bool swap_pause = false;
-            bool cycle_tic = false;
-            for (auto & input : inputs) {
-                if(!(localTime.isPaused())) {
-                    eventManager->raise(new UserInput(characters.at(idx++), input));
+            realTime.setTime();
+            localTime.setTime();
+        }
+
+        while (1)
+        {
+            realTime.setTime();
+            // set the new local time with each loop
+            localTime.setTime();
+
+            {
+                std::unique_lock<std::mutex> lock(m);
+                int idx = 0;
+                bool swap_pause = false;
+                bool cycle_tic = false;
+                for (auto & input : inputs) {
+                    if(!(localTime.isPaused())) {
+                        eventManager->raise(new UserInput(characters.at(idx++), input));
+                    }
                 }
-            }
-            eventManager->handleEvents();
-            if(!(localTime.isPaused())) {
-                objectManager->updateObjects();
-            }
+                eventManager->handleEvents();
+                if(!(localTime.isPaused())) {
+                    objectManager->updateObjects();
+                }
 
-            // window.clear(sf::Color::Black);
+                // window.clear(sf::Color::Black);
 
-            // for (auto & [key, value] : objectManager->getObjects()) {
-            //     window.draw(*value);
-            // }
-            // window.display();
+                // for (auto & [key, value] : objectManager->getObjects()) {
+                //     window.draw(*value);
+                // }
+                // window.display();
+            }
         }
     }
     return 0;
